4532a79edaeebfdd75315d2817751f2514121880
NathyG2524 <Nathygirma2524@gmail.com>
2021-12-02 09:11:43 -0800

    betty fixed

--- monty.c
+++ monty.c
@@ -2,74 +2,71 @@
 
 int main(int argc, char *argv[])
 {
-    int cmd;
-    char *size, *token;
-    int line_count = 0;
-    stack_t *inst = NULL;
-    unsigned int line = 1;
-    ssize_t n_read;
+	int cmd;
+	char *size, *token;
+	int line_count = 0;
+	stack_t *inst = NULL;
+	unsigned int line = 1;
+	ssize_t n_read;
 
-
-
-    if(argc != 2)
-    {
-        printf("Usage: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    cmd = open(argv[1], O_RDONLY);
-    if(cmd == -1)
-    {
-        printf("Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    size = malloc(sizeof(char)*10000);
-    if (!size)
-    {
-        return (0);
-    }
-    n_read = read(cmd, size, 10000);
-    if (n_read == -1)
+	if (argc != 2)
+	{
+		printf("Usage: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	cmd = open(argv[1], O_RDONLY);
+	if (cmd == -1)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	size = malloc(sizeof(char) * 10000);
+	if (!size)
+	{
+		return (0);
+	}
+	n_read = read(cmd, size, 10000);
+	if (n_read == -1)
 	{
 		free(size);
 		close(cmd);
 		exit(EXIT_FAILURE);
 	}
-    
-    token = strtok(size, "\n ");
-    while(token != NULL)
-    {
-        if(line_count == 1)
-        {
-            push(&inst, line, token);
-            line_count = 0;            
-            token = strtok(NULL, "\n ");
-            line++;           
-            continue;
-        }
-        else if (strcmp(token, "push") == 0)
-        {
-            line_count = 1;
-            token = strtok(NULL, "\n ");            
-            continue;
-        }        
-        else
-        {
-            if (opCode(token) != 0)
-            {
-                opCode(token)(&inst, line);
-            }
-            else {
-                free_dlist(&inst);
-                printf("L%d: unknown instruction %s\n", line, token);
-                exit(EXIT_FAILURE);
-            }
-        }
-        line++;
-        token = strtok(NULL, "\n ");        
-
-    }
-    free_dlist(&inst);
-    free(size);
-    close(cmd);
-    return (0);
+	token = strtok(size, "\n ");
+	while (token != NULL)
+	{
+		if (line_count == 1)
+		{
+			push(&inst, line, token);
+			line_count = 0;
+			token = strtok(NULL, "\n ");
+			line++;
+			continue;
+		}
+		else if (strcmp(token, "push") == 0)
+		{
+			line_count = 1;
+			token = strtok(NULL, "\n ");
+			continue;
+		}
+		else
+		{
+			if (opCode(token) != 0)
+			{
+				opCode(token)(&inst, line);
+			}
+			else
+			{
+				free_dlist(&inst);
+				printf("L%d: unknown instruction %s\n", line, token);
+				exit(EXIT_FAILURE);
+			}
+		}
+		line++;
+		token = strtok(NULL, "\n ");
+	}
+	free_dlist(&inst);
+	free(size);
+	close(cmd);
+	return (0);
 }
