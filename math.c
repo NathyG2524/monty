da849e516eab29c21b445e5e4a6a84c70290d126
NathyG2524 <Nathygirma2524@gmail.com>
2021-12-03 23:17:59 -0800

    math

--- math.c
+++ math.c
@@ -28,7 +28,7 @@ void nop(stack_t **inst, unsigned int line_number)
 	(void) line_number;
 }
 /**
- * _sub - adds nodes to the top of the list
+ * _sub - sub nodes to the top of the list
  * @inst: pointer to head of list
  * @line_number: line count position
  */
@@ -37,7 +37,7 @@ void _sub(stack_t **inst, unsigned int line_number)
 
         if (dlistint_len(*inst) < 2)
         {
-                fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+                fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
                 exit(EXIT_FAILURE);
         }
         (*inst)->next->n -= (*inst)->n;
@@ -46,7 +46,7 @@ void _sub(stack_t **inst, unsigned int line_number)
         (*inst)->prev = NULL;
 }
 /**
- * _mul - adds nodes to the top of the list
+ * _mul - mul nodes to the top of the list
  * @inst: pointer to head of list
  * @line_number: line count position
  */
@@ -55,11 +55,33 @@ void _mul(stack_t **inst, unsigned int line_number)
 
         if (dlistint_len(*inst) < 2)
         {
-                fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+                fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
                 exit(EXIT_FAILURE);
         }
         (*inst)->next->n *= (*inst)->n;
         (*inst) = (*inst)->next;
         free((*inst)->prev);
         (*inst)->prev = NULL;
+}
+/**
+ * _div - div nodes to the top of the list
+ * @inst: pointer to head of list
+ * @line_number: line count position
+ */
+void _div(stack_t **inst, unsigned int line_number)
+{
+
+        if (dlistint_len(*inst) < 2)
+        {
+                fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+	if ((*inst)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		cleaner();
+		exit(EXIT_FAILURE);
+	}
+        (*inst)->next->n /= (*inst)->n;
+	delete_index(stack, 0);
 }
