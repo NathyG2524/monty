5ecd5e688caa8554a13c60fd3156bfa6e3092ba5
NathyG2524 <Nathygirma2524@gmail.com>
2021-12-04 00:10:31 -0800

    math 2

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ int delete_index(stack_t **head, unsigned int index);
 void _add(stack_t **inst, unsigned int line_number);
 void _mul(stack_t **inst, unsigned int line_number);
 void _sub(stack_t **inst, unsigned int line_number);
+void mod(stack_t **inst, unsigned int line_number);
 void _div(stack_t **inst, unsigned int line_number);
 void nop(stack_t **inst, unsigned int line_number);
 #endif
